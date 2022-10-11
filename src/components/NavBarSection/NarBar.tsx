deca8f1acb89e58e121af033d65257df1b604ecb
Rachel-Fang <85208717+Rachel-Fang@users.noreply.github.com>
2022-10-11 17:28:11 +1100

    fix indent error (#6)
    Co-authored-by: Rachel Fang <yanhanfan828@gmail.com>

---
+++ src/components/NavBarSection/NarBar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+// import Logo from './Navigation/Logo';
+// import MenuLinks from "./Navigation/MenuLinks";
+// import MenuToggle from "./Navigation/MenuToggle";
+import NavBarContainer from "./NavBarContainer";
+
+const NavBar = (props) => {
+
+    return (
+        <NavBarContainer {...props}>
+            
+        </NavBarContainer>
+    )
+}
+
+export default NavBar
