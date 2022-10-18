d619584da067997d9a06dab7de82679898c8c161
Eden Cheng <cljeden@gmail.com>
2022-10-18 14:10:51 +1100

    merge main branch

--- src/components/MainPage/FeaturedMovie/FeaturedMovie.tsx
+++ src/components/MainPage/FeaturedMovie/FeaturedMovie.tsx
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-import styled from "styled-components";
-import { SectionHeaderContainer, SectionTitle } from "../Containers";
-import SeeMore from "../SeeMore";
-import Movies from "./Movies";
-
-const SectionContainer = styled.div`
-  width: 840px;
-  padding: 10px;
-`;
-
-const FeaturedMovie = () => {
-    return (
-        <SectionContainer>
-            <SectionHeaderContainer>
-                <SectionTitle>Featured Movie</SectionTitle>
-                <SeeMore />
-            </SectionHeaderContainer>
-            <Movies />
-        </SectionContainer>
-    );
-};
-
-export default FeaturedMovie;
-||||||| 8850e32
-=======
 import styled from "styled-components";
 import { SectionHeaderContainer, SectionTitle } from "../Containers";
 import SeeMore from "../SeeMore";
@@ -35,16 +9,15 @@ const SectionContainer = styled.div`
 `;
 
 const FeaturedMovie = () => {
-    return (
-        <SectionContainer>
-            <SectionHeaderContainer>
-                <SectionTitle>Featured Movie</SectionTitle>
-                <SeeMore />
-            </SectionHeaderContainer>
-            <Movies />
-        </SectionContainer>
-    );
+	return (
+		<SectionContainer>
+			<SectionHeaderContainer>
+				<SectionTitle>Featured Movie</SectionTitle>
+				<SeeMore />
+			</SectionHeaderContainer>
+			<Movies />
+		</SectionContainer>
+	);
 };
 
 export default FeaturedMovie;
->>>>>>> c82736b322d3629103b38d9d28bf52fec281b7e0
--- src/components/MainPage/FeaturedMovie/MovieThumbnail.tsx
+++ src/components/MainPage/FeaturedMovie/MovieThumbnail.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Box, Button, Image, Text } from "@chakra-ui/react";
 import IMDBRanking from "../IMDBRanking";
 
@@ -39,7 +38,16 @@ const MovieThumbnail = ({
 				/>
 			</Box>
 			<Box width="190px" height="40px">
-				<Text fontWeight="700" fontSize="text6" lineHeight="20px" fontFamily="secondary" color="gray.900" marginTop="16px">{title}</Text>
+				<Text
+					fontWeight="700"
+					fontSize="text6"
+					lineHeight="20px"
+					fontFamily="secondary"
+					color="gray.900"
+					marginTop="16px"
+				>
+					{title}
+				</Text>
 			</Box>
 			<Box marginTop="9px">
 				<IMDBRanking gap={"55px"} imdb={imdb} thumbsUp={thumbsUp} color="gray.900" />
@@ -62,78 +70,3 @@ const MovieThumbnail = ({
 };
 
 export default MovieThumbnail;
-||||||| 8850e32
-=======
-import { Box, Button, Image, Text } from "@chakra-ui/react";
-import IMDBRanking from "../IMDBRanking";
-
-interface IMovieThumbnailProps {
-    src: string;
-    title: string;
-    imdb: string;
-    thumbsUp: string;
-    watch: string;
-}
-
-const MovieThumbnail = ({
-    src,
-    title,
-    imdb,
-    thumbsUp,
-    watch,
-}: IMovieThumbnailProps): JSX.Element => {
-    return (
-        <Box marginTop="44px" pos="relative">
-            <Image src={src} width="190px" height="250px" />
-            <Box
-                bg="rgba(156, 163, 175, 0.5)"
-                width="30px"
-                height="29.21px"
-                borderRadius="50%"
-                backdropFilter="blur(10px)"
-                pos="absolute"
-                top="16px"
-                right="16px"
-            >
-                <Image
-                    src="/heart.svg"
-                    width="16px"
-                    height="13.3px"
-                    color="gray.100"
-                    margin="8.76px 7px 7.15px"
-                />
-            </Box>
-            <Box width="190px" height="40px">
-                <Text
-                    fontWeight="700"
-                    fontSize="text6"
-                    lineHeight="20px"
-                    fontFamily="secondary"
-                    color="gray.900"
-                    marginTop="16px"
-                >
-                    {title}
-                </Text>
-            </Box>
-            <Box marginTop="9px">
-                <IMDBRanking gap={"55px"} imdb={imdb} thumbsUp={thumbsUp} color="gray.900" />
-            </Box>
-            <Box marginTop="29px" display="flex" alignItems="center" justifyContent="center">
-                <Button
-                    padding="4px 30px"
-                    bg="gray.200"
-                    color="blue.500"
-                    fontSize="12px"
-                    fontWeight="700"
-                    lineHeight="24px"
-                    fontFamily="secondary"
-                >
-                    {watch}
-                </Button>
-            </Box>
-        </Box>
-    );
-};
-
-export default MovieThumbnail;
->>>>>>> c82736b322d3629103b38d9d28bf52fec281b7e0
