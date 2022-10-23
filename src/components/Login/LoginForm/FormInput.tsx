7653ab0f0622e9d9935737dd469cf59b79258e7c
Rachel Fang <85208717+Rachel-Fang@users.noreply.github.com>
2022-10-23 15:13:06 +1100

    Tjm 75 login (#23)
    * feat: add login modal
    
    * feat: add login modal
    
    * chore: tjm-75 use state store email and password
    
    * chore: prettier change format
    
    Co-authored-by: Rachel Fang <yanhanfan828@gmail.com>

---
+++ src/components/Login/LoginForm/FormInput.tsx
@@ -0,0 +1,33 @@
+import { Input } from "@chakra-ui/react";
+
+const placeHolderStyle = {
+    fontFamily: "secondary",
+    fontSize: "text4",
+    fontWeight: "400",
+    color: "gray.400",
+};
+
+interface IFormInput {
+    name: string;
+    placeholder: string;
+    type: string;
+    value: string;
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+const FormInput = ({ onChange, ...rest }: IFormInput) => {
+    return (
+        <Input
+            {...rest}
+            _placeholder={placeHolderStyle}
+            w="359px"
+            h="50px"
+            borderColor="gray.400"
+            pl="21px"
+            pt="15px"
+            pb="15px"
+            onChange={onChange}
+        />
+    );
+};
+
+export default FormInput;
