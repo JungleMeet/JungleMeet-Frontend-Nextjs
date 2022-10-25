e6b80013efd4af4f2b5bbff08439d9e57bd403ed
elultra <102572669+elultra@users.noreply.github.com>
2022-10-25 21:06:29 +1000

    Tjm 72 dicussion header review header (#29)
    * feat:discussion header tjm72
    
    * "fix:reform discussion header components tjm72"
    
    * feat:review header tjm72

---
+++ src/pages/reviews.tsx
@@ -0,0 +1,14 @@
+import PageWrapper from "@/components/PageWrapper";
+import ReviewHeader from "@/components/ReviewPage/ReviewHeader";
+import ReviewFilter from "@/components/ReviewPage/ReviewFilter";
+
+const Discussions = () => {
+    return (
+        <PageWrapper>
+            <ReviewHeader />
+            <ReviewFilter />
+        </PageWrapper>
+    );
+};
+
+export default Discussions;
