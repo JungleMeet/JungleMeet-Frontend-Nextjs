92c9ddd6042322eaedd324f814e84b3205a13150
sebrinax <you@example.com>
2022-10-26 23:19:39 +1100

    feat: minor change TJM-32

--- src/components/MovieDetailPage/MovieDetail.tsx
+++
@@ -1,3 +0,0 @@
-// import { Box, Flex, Text, useColorModeValue, Image } from "@chakra-ui/react";
-// import FeaturedCasts from "@/coponents/MovieDetailPage/FeaturedCasts";
-// import PopularReview from "@/coponents/MovieDetailPage/PopularReview";
