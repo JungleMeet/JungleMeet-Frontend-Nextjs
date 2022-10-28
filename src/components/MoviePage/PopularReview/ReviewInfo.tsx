3bc5f0bfd1f103d79e0cce282f4355f9122b4ec6
Rachel Fang <yanhanfan828@gmail.com>
2022-10-28 15:13:25 +1100

    feat: add clickable upcoming movies

--- src/components/MoviePage/PopularReview/ReviewInfo.tsx
+++ src/components/MoviePage/PopularReview/ReviewInfo.tsx
@@ -2,30 +2,37 @@ import { Flex, Text, Link, Spacer, Box, Divider, Image } from "@chakra-ui/react"
 import { ReviewsContainer } from "./ReviewsContainer";
 import React from "react";
 
-
 interface ReviewInfoProps {
     name: string;
     date: Date;
     helpful: number;
     comments: number;
     description: string;
-    
 }
 
 const ReviewInfoProps: React.FC<ReviewInfoProps> = (props) => {
     return (
         <ReviewsContainer>
-            <Box >
-                <Box pb="13px" lineHeight="lh32" >
+            <Box>
+                <Box pb="13px" lineHeight="lh32">
                     <Flex alignItems="center">
                         <Box display="flex">
                             <Text>
-                                <Link textColor= "blue.500" mr="5px" fontSize="18px" fontWeight="600" lineHeight="32px">
+                                <Link
+                                    textColor="blue.500"
+                                    mr="5px"
+                                    fontSize="18px"
+                                    fontWeight="600"
+                                    lineHeight="32px"
+                                >
                                     {props.name}
-                                </Link>&nbsp;
+                                </Link>
+                &nbsp;
                             </Text>
 
-                            <Text textColor="gray.400" fontSize="16px" fontWeight="400">{props.date.toLocaleDateString("en-AU")}</Text>
+                            <Text textColor="gray.400" fontSize="16px" fontWeight="400">
+                                {props.date.toLocaleDateString("en-AU")}
+                            </Text>
                         </Box>
                         <Spacer />
                         <Box
@@ -40,19 +47,17 @@ const ReviewInfoProps: React.FC<ReviewInfoProps> = (props) => {
                                 width="16px"
                                 height="16px"
                                 color="gray.900"
-                                margin="8.76px 7px 7.15px" >
-                            </Image>
-                            <Text textColor="red.500"> {props.helpful} Helpful </Text>                          
+                                margin="8.76px 7px 7.15px"
+                            ></Image>
+                            <Text textColor="red.500"> {props.helpful} Helpful </Text>
                         </Box>
                     </Flex>
-                
+
                     <Box fontSize="18px" lineHeight="24px" marginTop="20px">
-                        <Text >
-                            {props.description}
-                        </Text>
+                        <Text>{props.description}</Text>
                     </Box>
 
-                    <Box display="flex" textColor="gray.600" marginTop="18.67px" >
+                    <Box display="flex" textColor="gray.600" marginTop="18.67px">
                         <Image
                             src="/commentsIcon.svg"
                             width="16.5px"
