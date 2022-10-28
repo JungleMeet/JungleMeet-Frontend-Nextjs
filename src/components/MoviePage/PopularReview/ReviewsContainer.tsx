283a7211dbb3f58707d621cf3b7b54b3911c299f
Rachel Fang <85208717+Rachel-Fang@users.noreply.github.com>
2022-10-28 21:05:00 +1100

    Tjm 108 set state for upcoming movies (#47)
    * feat: add nowplaying connection
    
    * feat: add nowplaying connection
    
    * feat: add upcoming connection
    
    * feat: add upcoming connection
    
    * fix: conflict
    
    * fix: conflict
    
    * fix: conflict
    
    * fix: callstack error
    
    * feat: add clickable upcoming movies
    
    * feat: add clickable upcoming movies
    
    * feat: add clickable upcoming movies
    
    Co-authored-by: Rachel Fang <yanhanfan828@gmail.com>

--- src/components/MoviePage/PopularReview/ReviewsContainer.tsx
+++ src/components/MoviePage/PopularReview/ReviewsContainer.tsx
@@ -1,10 +1,9 @@
 import styled from "styled-components";
 
 export const ReviewsContainer = styled.div`
-    background-color: #ffffff;
-    width: 100%;
-    margin: auto;
-    margin-bottom: 50px;
-    margin-top: 30px
-   
+  background-color: #ffffff;
+  width: 100%;
+  margin: auto;
+  margin-bottom: 50px;
+  margin-top: 30px;
 `;
