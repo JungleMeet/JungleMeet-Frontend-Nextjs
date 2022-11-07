c0655dcdf905f933a5bd020ffacafde331a8c38e
Rachel Fang <yanhanfan828@gmail.com>
2022-11-07 14:10:23 +1100

    feat: add multiLingual to allpages

--- src/pages/reviews.tsx
+++ src/pages/reviews.tsx
@@ -1,6 +1,15 @@
 import PageWrapper from "@/components/PageWrapper";
 import ReviewHeader from "@/components/ReviewPage/ReviewHeader";
 import ReviewFilter from "@/components/ReviewPage/ReviewFilter";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+export async function getStaticProps({ locale }) {
+    return {
+        props: {
+            ...(await serverSideTranslations(locale, ["home"])),
+        },
+    };
+}
 
 const Discussions = () => {
     return (
