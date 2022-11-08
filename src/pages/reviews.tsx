d2d37ce3e03a5a537e95d18424a30e302c566505
elultra <zhuceyong666@outlook.com>
2022-11-08 23:18:38 +1000

    fix: update back to last work

--- src/pages/reviews.tsx
+++ src/pages/reviews.tsx
@@ -1,6 +1,5 @@
 import PageWrapper from "@/components/PageWrapper";
-import ReviewHeader from "@/components/ReviewPage/ReviewHeader";
-import ReviewFilter from "@/components/ReviewPage/ReviewFilter";
+import ReviewPage from "@/components/ReviewPage/ReviewPage";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 export async function getStaticProps({ locale }) {
@@ -11,13 +10,12 @@ export async function getStaticProps({ locale }) {
     };
 }
 
-const Discussions = () => {
+const Reviews = () => {
     return (
         <PageWrapper>
-            <ReviewHeader />
-            <ReviewFilter />
+            <ReviewPage/>
         </PageWrapper>
     );
 };
 
-export default Discussions;
+export default Reviews;
