fd14a14322b4f71a895511425057fe147ee0ad58
haoxuan001 <34461452+haoxuan001@users.noreply.github.com>
2022-11-12 18:36:56 +1000

    fix: add ID for reviewPosts fix replies icon

--- src/components/MoviePage/PopularReview/ReviewPosts.tsx
+++ src/components/MoviePage/PopularReview/ReviewPosts.tsx
@@ -2,20 +2,27 @@ import React from "react";
 import { getCommentsByCondition } from "@/utils/axiosCommentApi";
 import { useState, useEffect } from "react";
 import ReviewInfo from "./ReviewInfo";
+import { useRouter } from "next/router";
 
 const ReviewPosts = () => {
     const [reviewList, setReviewList] = useState([]);
-
+    const router = useRouter();
+    const { id }: any = router.query;
     useEffect(() => {
         const fetchComments = async () => {
-            try {
-                const res = await getCommentsByCondition("6355ec010dfda61162b3bec0", "createdAt", 3, 0);
-                setReviewList(res.data);
-                console.log("commentInfo", res.data);
-            } catch (err) {
-                return err;
+            const res = await getCommentsByCondition(id, "createdAt", 3, 0);
+            if (res.data.length === 0) {
+                () => {};
+            } else {
+                try {
+                    setReviewList(res.data);
+                    console.log("commentInfo", res.data);
+                } catch (err) {
+                    return err;
+                }
             }
         };
+
         fetchComments();
     }, []);
     return (
@@ -32,6 +39,7 @@ const ReviewPosts = () => {
                         views={viewNumber}
                         comments={commentCount}
                         description={content}
+                        id={""}
                     />
                 );
             }
