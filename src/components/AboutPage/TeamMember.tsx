09613eb7a902a3dab967cf80ec36b673dfb8f66b
elultra <102572669+elultra@users.noreply.github.com>
2022-11-15 18:14:07 +1000

    Tjm 156 review page (#86)
    * fix: update back to last work
    
    * feat: review list
    
    * feat: recursive comment
    
    * fix: review header
    
    * fix: review page

--- src/components/AboutPage/TeamMember.tsx
+++ src/components/AboutPage/TeamMember.tsx
@@ -1,25 +1,25 @@
-import { Box, Text, Flex} from "@chakra-ui/react";
+import { Box, Text, Flex } from "@chakra-ui/react";
 import React from "react";
 
-function TeamMember(props:any) { 
+function TeamMember(props: any) {
     return (
         <Box mt="55px" ml="28px" mr="2px" alignItems="center" mb="40px">
             <Flex gap="5px">
                 <Text
-                    color="#000"  
-                    fontFamily="Inter" 
+                    color="#000"
+                    fontFamily="Inter"
                     fontStyle="normal"
-                    fontWeight="700" 
+                    fontWeight="700"
                     fontSize="20px"
                     lineHeight="28px"
                 >
                     {props.memberName} -
                 </Text>
                 <Text
-                    color="rose.900" 
-                    fontFamily="Inter" 
+                    color="rose.900"
+                    fontFamily="Inter"
                     fontStyle="normal"
-                    fontWeight="700" 
+                    fontWeight="700"
                     fontSize="20px"
                     lineHeight="28px"
                 >
@@ -28,21 +28,20 @@ function TeamMember(props:any) {
             </Flex>
             <Text
                 mt="25"
-                color="#000" 
-                fontFamily="Inter" 
+                color="#000"
+                fontFamily="Inter"
                 fontStyle="normal"
-                fontWeight="400" 
+                fontWeight="400"
                 fontSize="16px"
                 lineHeight="20px"
             >
-                Email: {props.Email}
-                <br/>
-                LinkedIn: {props.LinkedIn}   
+        Email: {props.Email}
+                <br />
+        LinkedIn: {props.LinkedIn}
                 {/* How to make it a link? */}
             </Text>
         </Box>
-    )
+    );
 }
-
 
 export default TeamMember;
