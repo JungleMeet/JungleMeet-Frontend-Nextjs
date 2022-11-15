28122629eab2a79a4bdb5afcd3f6e85d60de31ad
JustinLin <36983969+yestinlin@users.noreply.github.com>
2022-11-15 20:20:33 +1100

    Tjm 135 top 10 post connect db (#88)
    * feat: signup and disscusion detail
    
    * feat: tjm-120-signup and discussion detail
    
    * feat: featched top10post

---
+++ src/components/MainPage/WeeklyTop10Post/WeeklyListItems.tsx
@@ -0,0 +1,43 @@
+import WeeklyList from "./WeeklyList";
+import React, { useEffect, useState } from "react";
+import { getPostsByView } from "@/utils/axiosPostApi";
+import { OrderedList, ListItem } from "@chakra-ui/react";
+
+const WeeklyListItems = () => {
+    const [postLists, setPostLists] = useState([]);
+
+    useEffect(() => {
+        const fetchPost = async () => {
+            try {
+                const res = await getPostsByView("views");
+                const { data } = res.data;
+
+                setPostLists(data);
+            } catch (err) {
+                return err;
+            }
+        };
+        fetchPost();
+    }, []);
+
+    return (
+        <OrderedList spacing="19px" pl="0">
+            {postLists.slice(0, 3).map(({ _id, title }) => {
+                return (
+                    <ListItem key={_id}>
+                        <WeeklyList postId={_id} title={title} icon={"/Fire.svg"} />{" "}
+                    </ListItem>
+                );
+            })}
+
+            {postLists.slice(4, 11).map(({ _id, title }) => {
+                return (
+                    <ListItem key={_id}>
+                        <WeeklyList postId={_id} title={title} icon={""} />{" "}
+                    </ListItem>
+                );
+            })}
+        </OrderedList>
+    );
+};
+export default WeeklyListItems;
