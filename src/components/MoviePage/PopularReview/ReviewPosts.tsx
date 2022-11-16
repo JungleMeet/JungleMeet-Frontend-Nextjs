9a2036a9bb564f9878524e9f5a0c1237c7273425
haoxuan001 <34461452+haoxuan001@users.noreply.github.com>
2022-11-16 21:52:57 +1000

    fix: connect seeMoreReviews data

--- src/components/MoviePage/PopularReview/ReviewPosts.tsx
+++ src/components/MoviePage/PopularReview/ReviewPosts.tsx
@@ -3,23 +3,25 @@ import { getCommentsByCondition } from "@/utils/axiosCommentApi";
 import { useState, useEffect } from "react";
 import ReviewInfo from "./ReviewInfo";
 import { useRouter } from "next/router";
+import SeeMoreReviews from "@/components/MainPage/SeeMoreReviews";
 
 const ReviewPosts = () => {
     const [reviewList, setReviewList] = useState([]);
+    const [topCommentsLength, setTopCommentsLength] = useState(0);
     const router = useRouter();
     const { id }: any = router.query;
     useEffect(() => {
         const fetchComments = async () => {
-            const res = await getCommentsByCondition(id, "createdAt", 3, 0);
-            if (res.data.length === 0) {
-                () => {};
-            } else {
-                try {
-                    setReviewList(res.data);
-                    console.log("commentInfo", res.data);
-                } catch (err) {
-                    return err;
+            try {
+                const res = await getCommentsByCondition(id, "createdAt", 3, 0);
+                if (res.data.length === 0) {
+                    () => {};
                 }
+                setReviewList(res.data.topComments);
+                setTopCommentsLength(res.data.length);
+                console.log("commentInfo", res.data);
+            } catch (err) {
+                return err;
             }
         };
 
@@ -29,12 +31,21 @@ const ReviewPosts = () => {
         <>
             {(reviewList || []).length > 0 &&
         reviewList?.map(
-            ({ _id, content, createdAt, author, likeCount, viewNumber, commentCount }) => {
+            ({
+                _id,
+                content,
+                createdAt,
+                author,
+                likeCount,
+                viewNumber,
+                commentCount,
+                topComment,
+            }) => {
                 return (
                     <ReviewInfo
                         key={_id}
                         author={author}
-                        date={createdAt}
+                        createdAt={createdAt}
                         likeCount={likeCount}
                         views={viewNumber}
                         comments={commentCount}
@@ -44,6 +55,7 @@ const ReviewPosts = () => {
                 );
             }
         )}
+            <SeeMoreReviews href={`/movies/reviews/${id}`} topCommentsLength={topCommentsLength} />
         </>
     );
 };
