8a496bb6492949bfe839daf4d8b6bfbac9231dd3
Rachel Fang <85208717+Rachel-Fang@users.noreply.github.com>
2022-11-21 17:31:44 +1100

    Fix allmovietype error (#107)
    * feat: add changepassword in prodile
    
    * feat: add i18 in login'
    
    * feat: add i18 in login'
    
    * fix: modify dropdown ui and add hightlight to content
    
    * feat: add navigate to see more activity
    
    * chore: add production url
    
    * chore: add production url
    
    * fix: typeerror
    
    * fix: typeerror
    
    Co-authored-by: Rachel Fang <yanhanfan828@gmail.com>

--- src/utils/axiosNotificationApi.tsx
+++ src/utils/axiosNotificationApi.tsx
@@ -3,7 +3,9 @@ import axios from "axios";
 const REQUEST_TIMEOUT = 10000;
 
 const axiosApi = axios.create({
-    baseURL: "http://localhost:3000/v1/notifications",
+    baseURL: process.env.NEXT_PUBLIC_SERVER
+        ? process.env.NEXT_PUBLIC_SERVER + "/v1/notifications"
+        : process.env.NEXT_PUBLIC_SERVER_ADD + "/v1/notifications",
     timeout: REQUEST_TIMEOUT,
 });
 
